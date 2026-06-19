cbb725559c7b1161be15ad742d4e5d396f446e26
Kamalesh <ecommers350@gmail.com>
2026-06-19 23:04:14 +0530

    created basic expo app

---
+++ client/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Redirect, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView} from 'react-native-gesture-handler'
+
+function AuthGuard() {
+  const { isSignedIn } = {isSignedIn: false}
+
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)" />
+  }else if(isSignedIn) {
+    return <Redirect href="/(tabs)" />
+  }
+}
+
+export default function RootLayout() {
+  return <GestureHandlerRootView style={{flex: 1}}>
+
+    <AuthGuard />
+
+    <Stack screenOptions={{headerShown: false}} >
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="chat/[id]" options={{ animation: 'slide_from_right' }} />
+    </Stack>
+    <StatusBar style="dark" />
+  </GestureHandlerRootView>
+}
